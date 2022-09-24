c9a3d49bc18f7838f3f31bf8d3a6f1583623c991
Tarik <tarikdotcom@gmail.com>
2022-09-25 00:42:00 +0300

    fix: deploy script branch naming

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:tarikpnr/tarikpnr.github.io.git main
+ git push -f git@github.com:tarikpnr/tarikpnr.github.io.git master
 
 cd -

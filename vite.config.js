dc1f9199c1ac5a5691aeb5116c25f80e0c9776e9
Tarik <tarikdotcom@gmail.com>
2022-09-24 23:58:39 +0300

    fix: potential fix, strictly declaring base path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // https://github.com/vitejs/vite/issues/238#issuecomment-633016902
+  base: './',
 });
